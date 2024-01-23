66b6a29bb836730da8ed7aef6d4cc48fa984e59d
Asep Irman <asepirman@antigravity.id>
2024-01-24 04:51:39 +0700

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,267 +1,91 @@
-'use client';
-
-import * as fal from '@fal-ai/serverless-client';
-import { useMemo, useState } from 'react';
-
-
-// @snippet:start(client.config)
-fal.config({
-  // credentials: 'FAL_KEY_ID:FAL_KEY_SECRET',
-  requestMiddleware: fal.withProxy({
-    targetUrl: '/api/fal/proxy', // the built-int nextjs proxy
-    // targetUrl: 'http://localhost:3333/api/fal/proxy', // or your own external proxy
-  }),
-});
-// @snippet:end
-
-// @snippet:start(client.result.type)
-type Image = {
-  url: string;
-  file_name: string;
-  file_size: number;
-};
-type Result = {
-  image: Image;
-};
-type FaceSwapResult = {
-  image: Image;
-};
-// @snippet:end
-
-type ErrorProps = {
-  error: any;
-};
-
-function Error(props: ErrorProps) {
-  if (!props.error) {
-    return null;
-  }
-  return (
-    <div
-      className="p-4 mb-4 text-sm text-red-800 rounded bg-red-50 dark:bg-gray-800 dark:text-red-400"
-      role="alert"
-    >
-      <span className="font-medium">Error</span> {props.error.message}
-    </div>
-  );
-}
-
-const DEFAULT_PROMPT = 'Man cyberpunk, synthwave night city, futuristic, high quality, highly detailed, high resolution, sharp, hyper realistic, extremely detailed';
-const DEFAULT_NEG_PROMPT = 'blurry, low resolution, bad, ugly, low quality, pixelated, interpolated, compression artifacts, noisey, grainy';
-let URL_RESULT = ''
-
+import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
-  // @snippet:start("client.ui.state")
-  // Input state
-  const [prompt, setPrompt] = useState<string>(DEFAULT_PROMPT);
-  const negative_prompt = DEFAULT_NEG_PROMPT;
-  const [imageFile, setImageFile] = useState<File | null>(null);
-  // Result state
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [result, setResult] = useState<Result | null>(null);
-  const [resultFaceSwap, setResultFaceSwap] = useState<FaceSwapResult | null>(null);
-  const [logs, setLogs] = useState<string[]>([]);
-  const [elapsedTime, setElapsedTime] = useState<number>(0);
-  // @snippet:end
-  const image = useMemo(() => {
-    if (!result) {
-      return null;
-    }
-    if (result.image) {
-      return result.image;
-    }
-    return null;
-  }, [result]);
-  const imageFaceSwap = useMemo(() => {
-    if (!resultFaceSwap) {
-      return null;
-    }
-    if (resultFaceSwap.image) {
-      return resultFaceSwap.image;
-    }
-    return null;
-  }, [resultFaceSwap]);
-
-  const reset = () => {
-    setLoading(false);
-    setError(null);
-    setResult(null);
-    setResultFaceSwap(null);
-    setLogs([]);
-    setElapsedTime(0);
-  };
-  const reset2 = () => {
-    setLoading(false);
-    setError(null);
-    // setLogs([]);
-    setElapsedTime(0);
-  };
-  const generateImage = async () => {
-    reset();
-    // @snippet:start("client.queue.subscribe")
-    setLoading(true);
-    const start = Date.now();
-    try {
-      const result: Result = await fal.subscribe(
-        'fal-ai/ip-adapter-face-id',
-        {
-          input: {
-            prompt,
-            face_image_url: imageFile,
-            negative_prompt
-          },
-          pollInterval: 5000, // Default is 1000 (every 1s)
-          logs: true,
-          onQueueUpdate(update) {
-            setElapsedTime(Date.now() - start);
-            if (
-              update.status === 'IN_PROGRESS' ||
-              update.status === 'COMPLETED'
-            ) {
-              setLogs((update.logs || []).map((log) => log.message));
-              console.log(update)
-            }
-          },
-        }
-      );
-      setResult(result);
-      URL_RESULT = result.image.url
-    } catch (error: any) {
-      setError(error);
-    } finally {
-      setLoading(false);
-      setElapsedTime(Date.now() - start);
-      generateImageSwap()
-    }
-    // @snippet:end
-  };
-
-
-  const generateImageSwap = async () => {
-    console.log(URL_RESULT)
-    reset2();
-    // @snippet:start("client.queue.subscribe")
-    setLoading(true);
-    const start = Date.now();
-    try {
-      const result: Result = await fal.subscribe(
-        'fal-ai/face-swap',
-        {
-          input: {
-            base_image_url: URL_RESULT,
-            swap_image_url: imageFile
-          },
-          pollInterval: 5000, // Default is 1000 (every 1s)
-          logs: true,
-          onQueueUpdate(update) {
-            setElapsedTime(Date.now() - start);
-            if (
-              update.status === 'IN_PROGRESS' ||
-              update.status === 'COMPLETED'
-            ) {
-              setLogs((update.logs || []).map((log) => log.message));
-            }
-          },
-        }
-      );
-      setResultFaceSwap(result);
-    } catch (error: any) {
-      setError(error);
-    } finally {
-      setLoading(false);
-      setElapsedTime(Date.now() - start);
-    }
-    // @snippet:end
-  };
   return (
-    <div className="min-h-screen dark:bg-gray-900 bg-gray-100">
-      <main className="container dark:text-gray-50 text-gray-900 flex flex-col items-center justify-center w-full flex-1 py-10 space-y-8">
-        <h1 className="text-4xl font-bold mb-8">
-          Hello <code className="font-light text-pink-600">fal</code>
-        </h1>
-        <div className="text-lg w-full">
-          <label htmlFor="prompt" className="block mb-2 text-current">
-            Image
-          </label>
-          <input
-            className="w-full text-lg p-2 rounded bg-black/10 dark:bg-white/5 border border-black/20 dark:border-white/10"
-            id="image_url"
-            name="image_url"
-            type="file"
-            placeholder="Choose a file"
-            accept="image/*;capture=camera"
-            onChange={(e) => setImageFile(e.target.files?.[0] ?? null)}
+    <main className="container flex flex-col items-center justify-between">
+      <div className="relative flex w-full overflow-x-hidden border-b-4 border-t-4 border-black bg-[#F9B800] font-bold z-20 pointer-events-none">
+        <div className="animate-marquee whitespace-nowrap py-2">
+            <span className="text-lg tracking-[-.15em]">artificial.intelligence</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">stabble-diffusion</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">SDXL</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">diffusion-edge</span><span className="mx-4">•</span>
+        </div>
+        <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-2">
+            <span className="text-lg tracking-[-.15em]">artificial.intelligence</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">stabble-diffusion</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">SDXL</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">diffusion-edge</span><span className="mx-4">•</span>
+        </div>
+      </div>
+
+      <div className="absolute w-full top-[36px] left-[-10px] z-10 pointer-events-none">
+          <Image
+            className="relative h-auto w-full"
+            src="/ornament2.png"
+            alt="icon"
+            width={376}
+            height={104}
+            priority
+          />
+      </div>
+
+      <div className="relative flex place-items-center mt-16 z-50">
+        <Image
+          className="relative"
+          src="/logo.svg"
+          alt="AI Logo"
+          width={344}
+          height={244}
+          priority
+        />
+      </div>
+
+      <div className="fixed container bottom-[52px] flex flex-col items-center justify-between w-full border-t-4 border-black bg-[#FE4A17] pt-12 pb-8 font-bold">
+        <div className="absolute top-[-86px] right-[20px] pointer-events-none">
+          <Image
+            className="relative"
+            src="/smile.png"
+            alt="icon"
+            width={112}
+            height={112}
+            priority
           />
         </div>
-        <div className="text-lg w-full">
-          <label htmlFor="prompt" className="block mb-2 text-current">
-            Prompt
-          </label>
-          <input
-            className="w-full text-lg p-2 rounded bg-black/10 dark:bg-white/5 border border-black/20 dark:border-white/10"
-            id="prompt"
-            name="prompt"
-            placeholder="Imagine..."
-            value={prompt}
-            autoComplete="off"
-            onChange={(e) => setPrompt(e.target.value)}
-            onBlur={(e) => setPrompt(e.target.value.trim())}
+        <div className="absolute top-[-106px] left-[13px] pointer-events-none">
+          <Image
+            className="relative"
+            src="/ornament1.png"
+            alt="icon"
+            width={151}
+            height={138}
+            priority
           />
         </div>
 
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            generateImage();
-          }}
-          className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold text-lg py-3 px-6 mx-auto rounded focus:outline-none focus:shadow-outline"
-          disabled={loading}
-        >
-          {loading ? 'Generating...' : 'Generate Image'}
-        </button>
+        <Link href="/generate">
+          <button
+            role="button"
+            aria-label="Click to perform an action"
+            className="flex cursor-pointer items-center border-4 border-black bg-[#FDDC07] px-6 py-3 font-bold text-2xl shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
+          >
+            CREATE NOW!
+          </button>
+          <div className="relative flex items-center justify-center mt-6">
+            <Image
+              className="relative"
+              src="/developed.svg"
+              alt="zirolu"
+              width={168}
+              height={20}
+              priority
+            />
+          </div>
+        </Link>
+      </div>
 
-        <Error error={error} />
-
-        <div className="w-full flex flex-col space-y-4">
-          <div className="mx-auto">
-            {image && (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img src={image.url} alt="" />
-            )}
-          </div>
-          <div className="mx-auto">
-            {imageFaceSwap && (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img src={imageFaceSwap.url} alt="" />
-            )}
-          </div>
-          <div className="space-y-2">
-            <h3 className="text-xl font-light">JSON Result</h3>
-            <p className="text-sm text-current/80">
-              {`Elapsed Time (seconds): ${(elapsedTime / 1000).toFixed(2)}`}
-            </p>
-            {/* <pre className="text-sm bg-black/70 text-white/80 font-mono h-60 rounded whitespace-pre overflow-auto w-full">
-              {result
-                ? JSON.stringify(result, null, 2)
-                : '// result pending...'}
-            </pre>
-            <pre className="text-sm bg-black/70 text-white/80 font-mono h-60 rounded whitespace-pre overflow-auto w-full">
-              {resultFaceSwap
-                ? JSON.stringify(resultFaceSwap, null, 2)
-                : '// result pending...'}
-            </pre> */}
-          </div>
-
-          <div className="space-y-2">
-            <h3 className="text-xl font-light">Logs</h3>
-            <pre className="text-sm bg-black/70 text-white/80 font-mono h-60 rounded whitespace-pre overflow-auto w-full">
-              {logs.filter(Boolean).join('\n')}
-            </pre>
-          </div>
+      <div className="fixed container bottom-0 flex w-full overflow-x-hidden border-b-4 border-t-4 border-black bg-[#F9B800] font-bold pointer-events-none">
+        <div className="animate-marquee3 whitespace-nowrap py-2">
+            <span className="text-lg tracking-[-.15em]">machine.learning</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">LoRA</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">image-to-image</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">affine.matrix</span><span className="mx-4">•</span>
         </div>
-      </main>
-    </div>
+        <div className="absolute top-0 animate-marquee4 whitespace-nowrap py-2">
+            <span className="text-lg tracking-[-.15em]">machine.learning</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">LoRA</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">image-to-image</span><span className="mx-4">•</span><span className="text-lg tracking-[-.15em]">affine.matrix</span><span className="mx-4">•</span>
+        </div>
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,30 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    animation: {
+      marquee: "marquee 5s linear infinite",
+      marquee2: "marquee2 5s linear infinite",
+      marquee3: "marquee3 5s linear infinite",
+      marquee4: "marquee4 5s linear infinite",
+    },
+    keyframes: {
+      marquee: {
+        "0%": { transform: "translateX(0%)" },
+        "100%": { transform: "translateX(-100%)" },
+      },
+      marquee2: {
+        "0%": { transform: "translateX(100%)" },
+        "100%": { transform: "translateX(0%)" },
+      },
+      marquee3: {
+        "0%": { transform: "translateX(-100%)" },
+        "100%": { transform: "translateX(0%)" },
+      },
+      marquee4: {
+        "0%": { transform: "translateX(0%)" },
+        "100%": { transform: "translateX(100%)" },
+      },
+    },
   },
   plugins: [],
 };
